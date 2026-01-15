2eb2e893e2b87475405c606e306ce5cc1d3e44a2
zwphyro <tofumaks@gmail.com>
2026-01-16 00:31:31 +0300

    add postgres connection

--- services/redirect-service/go.mod
+++ services/redirect-service/go.mod
@@ -17,6 +17,12 @@ require (
 	github.com/go-playground/validator/v10 v10.30.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.8.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -36,7 +42,9 @@ require (
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
-	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
 	golang.org/x/tools v0.40.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
+	gorm.io/driver/postgres v1.6.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
 )
--- services/redirect-service/main.go
+++ services/redirect-service/main.go
@@ -3,31 +3,74 @@ package main
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+	"golang.org/x/sync/singleflight"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var RedisClient = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "",
-	DB:       0,
-})
+var (
+	DB          *gorm.DB
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	RedirectGroup = singleflight.Group{}
+)
 
-func redirect(ctx *gin.Context) {
+type RedirectLink struct {
+	gorm.Model
+	Short string
+	Link  string
+}
+
+func Redirect(ctx *gin.Context) {
 	id := ctx.Param("id")
 
-	redirectLink, error := RedisClient.Get(context.Background(), id).Result()
-	if error != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{"error": "Not Found"})
+	redirectLink, err := RedisClient.Get(context.Background(), id).Result()
+
+	if err == nil {
+		ctx.Redirect(http.StatusFound, redirectLink)
 		return
 	}
 
-	ctx.Redirect(http.StatusFound, redirectLink)
+	switch err {
+	case redis.Nil:
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Not Found"})
+		return
+	case context.DeadlineExceeded:
+		ctx.JSON(http.StatusRequestTimeout, gin.H{"error": "Request Timeout"})
+		return
+	}
+
+	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+}
+
+func TimeoutMiddleware(timeout time.Duration) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		timeoutContext, cancel := context.WithTimeout(ctx.Request.Context(), timeout)
+
+		defer cancel()
+
+		ctx.Request = ctx.Request.WithContext(timeoutContext)
+		ctx.Next()
+	}
 }
 
 func main() {
+	dns := "host=localhost user=admin password=root dbname=redirect_db port=5432 sslmode=disable"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dns), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
 	app := gin.Default()
-	app.GET("/:id", redirect)
+	app.Use(TimeoutMiddleware(1 * time.Second))
+	app.GET("/:id", Redirect)
 	app.Run(":8080")
 }
