8fae85c10928849d25dcf3b5e1519fb2a4fab2f9
zwphyro <tofumaks@gmail.com>
2026-05-02 02:31:47 +0300

    update models

--- apps/api-backend/src/auth/models.py
+++ apps/api-backend/src/auth/models.py
@@ -1,6 +1,11 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.db import Base, id, created_at, updated_at
+
+if TYPE_CHECKING:
+    from src.redirect_link.models import RedirectLink
 
 
 class User(Base):
@@ -10,3 +15,5 @@ class User(Base):
 
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
+
+    redirect_links: Mapped[list["RedirectLink"]] = relationship(back_populates="user")
